249d90383834d239c8b0386347fdb89d00a9db0e
ffattahh <fattahrap@gmail.com>
2026-06-08 22:49:24 +0700

    Upload Modul 7

---
+++ 2311102019_FattahRizqyAdhipratama/lib/login_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'register_page.dart';
+import 'home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  Future login() async {
+    try {
+
+      await Supabase.instance.client.auth.signInWithPassword(
+        email: email.text,
+        password: password.text,
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const HomePage(),
+        ),
+      );
+
+    } catch (e) {
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString()))
+      );
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Coffee Order Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+
+            TextField(
+              controller: email,
+              decoration: const InputDecoration(
+                labelText: "Email",
+              ),
+            ),
+
+            TextField(
+              controller: password,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: login,
+              child: const Text("Login"),
+            ),
+
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const RegisterPage(),
+                  ),
+                );
+              },
+              child: const Text("Belum punya akun? Register"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
